209e52a5a8928dcffc91cdeb99d9973e65adc147
Joseph Anderson <devanderson0412@gmail.com>
2022-07-09 02:54:09 -0500

    eth usdt withdraw function defined

--- src/api/user.py
+++ src/api/user.py
@@ -17,7 +17,7 @@ from src.schemas.auth import TokenPayload
 from uniswap import Uniswap
 
 from config import cfg
-from src.utils.web3 import compare_eth_address, get_current_gas_price, get_transaction_eth_value, get_transaction_token_value, wait_transaction_receipt
+from src.utils.web3 import compare_eth_address, get_current_gas_price, send_eth_token_to, get_transaction_eth_value, get_transaction_token_value, wait_transaction_receipt
 
 eth = "0x0000000000000000000000000000000000000000"
 usdt = cfg.ETH_USDT_ADDRESS
@@ -48,16 +48,27 @@ class UserAPI(Function):
             "rollback": user.rollback
           }
 
-        @router.get('/price/eth/input', summary='Returns the amount of USDT you get for exact eth')
-        async def get_price_eth_input(amount: float) -> float:
+        @router.get('/price/eth/usdt/input', summary='Returns the amount of USDT you get for exact ETH')
+        async def get_price_eth_usdt_input(amount: float) -> float:
           fee = get_current_gas_price() * int(cfg.ETH_SWAP_FEE)
           print(fee / 10**18)
           return (float(self.uniswap.get_price_input(eth, usdt, int(amount*(10**18)) + fee)))/10**6
 
-        @router.get('/price/eth/output', summary='Returns the amount of USDT you get for exact eth')
-        async def get_price_eth_input(amount: float) -> float:
+        @router.get('/price/eth/usdt/output', summary='Returns the amount of ETH need to deposit for exact USDT')
+        async def get_price_eth_usdt_output(amount: float) -> float:
           fee = get_current_gas_price() * int(cfg.ETH_SWAP_FEE)
           return (float(self.uniswap.get_price_output(eth, usdt, int(amount*(10**6)))) + fee)/10**18
+
+        @router.get('/price/usdt/eth/input', summary='Returns the amount of ETH you get for exact USDT')
+        async def get_price_usdt_eth_input(amount: float) -> float:
+          fee = get_current_gas_price() * int(cfg.ETH_SWAP_FEE)
+          print(fee / 10**18)
+          return (float(self.uniswap.get_price_input(usdt, eth, int(amount*(10**6)))) + fee)/10**18
+
+        @router.get('/price/usdt/eth/output', summary='Returns the amount of USDT need to deposit for exact ETH')
+        async def get_price_usdt_eth_output(amount: float) -> float:
+          fee = get_current_gas_price() * int(cfg.ETH_SWAP_FEE)
+          return (float(self.uniswap.get_price_output(usdt, eth, int(amount*(10**18))+ fee)))/10**6
 
         @router.post('/deposit/eth', summary="Deposit eth to user account")
         async def deposit_eth(tx_hash: str = Query(default = None, regex='0x[a-z0-9]{64}'), payload: TokenPayload = Depends(get_current_user_from_oauth), session:Session = Depends(get_db_session)) -> float:
@@ -100,7 +111,7 @@ class UserAPI(Function):
 
           return new_history
 
-        @router.post('/deposit/usdt', summary="Deposit eth to user account")
+        @router.post('/deposit/usdt', summary="Deposit USDT to user account")
         async def deposit_usdt(tx_hash: str = Query(default = None, regex='0x[a-z0-9]{64}'), payload: TokenPayload = Depends(get_current_user_from_oauth), session:Session = Depends(get_db_session)) -> float:
 
           if tx_hash == None:
@@ -143,5 +154,63 @@ class UserAPI(Function):
           user_balance.balance += received
 
           return new_history
+
+        @router.post('/withdraw/eth', summary="Withdraw money with eth")
+        async def withdraw_eth(amount: float = Query(default=0), wallet: str = Query(default=None, regex='0x[A-Za-z0-9]{40}'), payload: TokenPayload = Depends(get_current_user_from_oauth), session:Session = Depends(get_db_session)) -> float:
+          if amount == 0:
+            raise HTTPException(
+              status_code=status.HTTP_400_BAD_REQUEST,
+              detail="You need to set amount more than 0"
+            )
+
+          user_balance: UserBalance = session.query(UserBalance).filter(UserBalance.user_id == payload.sub).one()
+
+          fee = get_current_gas_price() * int(cfg.ETH_SWAP_FEE)
+          total_eth = self.uniswap.get_price_input(usdt, eth, (user_balance.balance) * 10**6) - fee
+
+          if amount > total_eth:
+            raise HTTPException(
+              status_code=status.HTTP_400_BAD_REQUEST,
+              detail="Much than real amount"
+            )
+
+          user_balance.balance -= float(self.uniswap.get_price_output(usdt, eth, amount*10**18) + fee)/10**6
+
+          tx = self.uniswap.make_trade_output(usdt, eth, amount*10**18, wallet)
+          receipt = wait_transaction_receipt(tx)
+
+          return receipt
+
+        @router.post('/withdraw/usdt', summary="Withdraw money with USDT")
+        async def withdraw_eth(amount: float = Query(default=0), wallet: str = Query(default=None, regex='0x[A-Za-z0-9]{40}'), payload: TokenPayload = Depends(get_current_user_from_oauth), session:Session = Depends(get_db_session)) -> float:
+          if amount == 0:
+            raise HTTPException(
+              status_code=status.HTTP_400_BAD_REQUEST,
+              detail="You need to set amount more than 0"
+            )
+
+          user_balance: UserBalance = session.query(UserBalance).filter(UserBalance.user_id == payload.sub).one()
+
+          fee = get_current_gas_price() * int(cfg.ETH_SWAP_FEE)
+          fee_usdt = float(self.uniswap.get_price_output(usdt, eth, fee))/10**6
+
+          if amount + fee_usdt > user_balance.balance:
+            raise HTTPException(
+              status_code=status.HTTP_400_BAD_REQUEST,
+              detail="Much than real amount"
+            )
+
+          try:
+            receipt = send_eth_token_to(cfg.ETH_USDT_ADDRESS, wallet, amount)
+          except Exception as ex:
+            print(ex)
+            raise HTTPException(
+              status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+              detail="Some error occured in server side"
+            )
+
+          user_balance.balance -= amount + fee_usdt
+          return receipt['blockHash'].hex()
+
 
         app.include_router(router)
--- src/utils/web3.py
+++ src/utils/web3.py
@@ -1,11 +1,18 @@
+from http.client import HTTPException
 from eth_abi import decode_abi
+from eth_account import Account
+from eth_utils import to_bytes
 from hexbytes import HexBytes
 from web3 import Web3
+from web3.middleware import construct_sign_and_send_raw_middleware
 from web3.auto import w3
 
 from config import cfg
+from src.abis.ERC20 import abi as ERC20_abi
 
 web3_eth = Web3(provider=Web3.HTTPProvider(cfg.ETH_RPC_URL))
+web3_eth.middleware_onion.add(construct_sign_and_send_raw_middleware(cfg.ETH_TREASURY_PRIVATE_KEY))
+web3_eth.eth.default_account = cfg.ETH_TREASURY_ADDRESS
 
 def compare_eth_address(address1: str, address2: str) -> bool:
   try:
@@ -49,3 +56,25 @@ def wait_transaction_receipt(tx_hash: HexBytes) -> object:
 
 def get_current_gas_price() -> int:
   return web3_eth.eth._gas_price()
+
+def send_eth_token_to(address: str, wallet: str, amount:int) -> object:
+  contract_address = Web3.toChecksumAddress(address)
+  treasury_address = Web3.toChecksumAddress(cfg.ETH_TREASURY_ADDRESS)
+  wallet_address = Web3.toChecksumAddress(wallet)
+
+  # try:
+  contract = web3_eth.eth.contract(contract_address, abi=ERC20_abi)
+  decimal = contract.functions.decimals().call()
+
+  amount_wei = int(amount * 10 ** decimal)
+  transaction = contract.functions.transfer(wallet_address, amount_wei).buildTransaction({
+    'type': '0x2',  # optional - defaults to '0x2' when dynamic fee transaction params are present
+    'from': treasury_address,  # optional if w3.eth.default_account was set with acct.address
+    'maxFeePerGas': 2000000000,  # required for dynamic fee transactions
+    'maxPriorityFeePerGas': 1000000000,  # required for dynamic fee transactions
+  })
+
+  hash_hex = web3_eth.eth.send_transaction(transaction)
+  receipt = wait_transaction_receipt(hash_hex)
+
+  return receipt
