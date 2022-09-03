9b18e0172a310ae6065e257625f080972f118520
Joseph <devanderson0412@gmail.com>
2022-09-03 15:01:06 -0700

    for host

---
+++ run.py
@@ -0,0 +1,18 @@
+import uvicorn
+
+original_callback = uvicorn.main.callback
+
+
+def callback(**kwargs):
+    from celery.contrib.testing.worker import start_worker
+    from src.celery import celery
+
+    with start_worker(celery, perform_ping_check=False, loglevel="info"):
+        original_callback(**kwargs)
+
+
+uvicorn.main.callback = callback
+
+
+if __name__ == "__main__":
+    uvicorn.main()
